6a0a09a7cf26299dfa8cccf9e8872e51264681ab
Finn Van Eyck <finn.vaneyck@student.arteveldehs.be>
2026-04-20 14:22:36 +0200

    vercel file aamgemaakt

--- server/app.ts
+++ server/app.ts
@@ -3,11 +3,11 @@ import path from "path";
 import routes from "./routes";
 
 const app: Application = express();
-const PORT: number = 3000;
+const PORT : number = parseInt(<string>process.env.PORT, 10) || 3000;
 
 //Routes importeren
 app.use("/", routes);
-
+app.set("views", path.join(__dirname, "views"));
 
 app.listen(PORT, () => {
   console.log(`Server draait op http://localhost:${PORT}`);
